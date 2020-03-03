616d0fe499e1507e6c513f6ab9ec970062566eb9
Matija <Matija@DESKTOP-MO9U8AO>
2020-03-03 15:44:28 +0100

    Initial project structure

---
+++ ag04-demo/Entities/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities
+{
+    public class PlayerData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ag04-demo/ag04-demo/Controllers/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ag04_demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public bool Get()
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
